bedf47ee5e96d8c6c7b85a1418b55bede16d7070
Jiang Baorui <850051568@qq.com>
2024-02-13 21:30:49 +0800

    init

---
+++ psinsenvi.m
@@ -0,0 +1,4 @@
+function [rpath, dpath, mytestflag] = psinsenvi()
+	rpath = 'D:\OneDrive - ahu.edu.cn\500_NUAA_dv\520_research field\522_trial\psins240101';
+	dpath = 'D:\OneDrive - ahu.edu.cn\500_NUAA_dv\520_research field\522_trial\psins240101\data\';
+	mytestflag = 0;
